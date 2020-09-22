d2308b41b5bbe12c0750ad3811734fc2646af800
unknown <lucasmatoscastelo@gmail.com>
2020-09-21 20:35:09 -0400

    download de todos os arquinos no dropbox

--- TesteDrop.R
+++ TesteDrop.R
@@ -1,4 +1,5 @@
 library(rdrop2)
+library(Rcpp)
 library(tidyverse)
 library(readxl)
 library(readr)
@@ -11,9 +12,15 @@ saveRDS(token, file = "token.rds")
 drop_acc() %>% data.frame()
 
 
-token <<- readRDS("droptoken.rds")
+token <<- readRDS("token.rds")
 
-drop_download(dtoken = token,"Produtividade.csv", overwrite = TRUE)
+drop_download(dtoken = token,"Produtividade.csv", overwrite = TRUE,"~/PainelSPOE/PainelSPOE")
 Produtividade = read_delim("Produtividade.csv", ";", escape_double = FALSE, trim_ws = TRUE)
 
+
+drop_download(dtoken = token,"BaseUnica.xlsx", overwrite = TRUE,"~/PainelSPOE/PainelSPOE")
+drop_download(dtoken = token,"Prisao.xlsx", overwrite = TRUE,"~/PainelSPOE/PainelSPOE")
+drop_download(dtoken = token,"homicidios18-19.xlsx", overwrite = TRUE,"~/PainelSPOE/PainelSPOE")
+drop_download(dtoken = token,"RouboSROP-3.csv", overwrite = TRUE,"~/PainelSPOE/PainelSPOE")
+drop_download(dtoken = token,"FurtoSROP-1-SemAcento1.csv", overwrite = TRUE,"~/PainelSPOE/PainelSPOE")
 
